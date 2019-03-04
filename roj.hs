fbe9e9bcd36f7f3ec57dd58e1233d712e9cb94e7
Johan Haag <johan.isacsson@hotmail.se>
2019-03-04 11:02:55 +0100

    adding flags

--- roj.hs
+++ roj.hs
@@ -32,18 +32,27 @@ INVARIANT:
 
 type NeighbourMines = Int
 
+
+{- Flag
+HOW:
+INVARIANT:
+
+-}
+
+type Flag = Bool
+
 {-
 The datatype Board represent the game field as a list of Positions that creates a square.
 INVARIANT n Stuff !
 -}
-type Board =[Square]
+type Board = [Square]
 
 data State = Revealed
            | Hidden deriving (Show, Eq)
 
 {-
 Square represents stuff -} --PERHAPS MOVE THIS ONE ABOVE BOARD?
-data Square = Square Position State Mine NeighbourMines deriving (Show,Eq)
+data Square = Square Position State Mine NeighbourMines Flag deriving (Show,Eq)
 
 
 {- mkBoard n
@@ -60,7 +69,7 @@ EXAMPLES: mkBoard 0 = []
 
 mkBoard :: Int -> Board
 
-mkBoard n = [(Square (x,y) Hidden False 0) 
+mkBoard n = [(Square (x,y) Hidden False 0 False) 
             | x <- [1..n], y <- [1..n]]
 
 {- insertMines mines board
@@ -77,7 +86,6 @@ EXAMPLES: insertMines [] [] = []
 -}
 
 insertMines :: [Position] -> Board -> Board
-
 insertMines [] board     = board
 insertMines (p:ps) []    = []
 insertMines (p:ps) board = insertMines ps (insertMine p board)
@@ -94,10 +102,9 @@ EXAMPLES: insertMine (1,6) [] = []
  -} 
 
 insertMine :: Position -> Board -> Board
-
 insertMine _ [] = []
-insertMine position (square@(Square pos _ _ _):squares)
-  | position == pos = (Square pos Hidden True 1) : squares
+insertMine position (square@(Square pos _ _ _ _):squares)
+  | position == pos = (Square pos Hidden True 1 False) : squares
   | otherwise = square : insertMine position squares
 
 
@@ -111,7 +118,6 @@ EXAMPLES: randomCoords 0 = []
 -}
 
 randomCoord :: Int -> IO Position
-
 randomCoord n = do
 
     x <- randomRIO (1, n)
@@ -133,7 +139,6 @@ EXAMPLES: prettyPrint "" =
 
 
 prettyPrint :: String -> IO ()
-
 prettyPrint n = putStrLn n
 
 
@@ -149,9 +154,8 @@ EXAMPLES: printBoard []  5  = ""
 
 
 printBoard :: Board -> Int -> String
-
 printBoard []  n  = []
-printBoard (x@(Square (x1,y1) _ _ _):xs) n
+printBoard (x@(Square (x1,y1) _ _ _ _):xs) n
   | y1 == 1 && x1 == 1 = "   " ++ (reverse $ printTop n) ++ "\n" ++ (show x1) ++ " | " ++ printSquare x ++ " " ++ printBoard xs n
   | y1 == 1 = if x1 < 10 then (show x1) ++ " | " ++ printSquare x ++ " " ++ printBoard xs n 
     else (show x1) ++ "| " ++ printSquare x ++ " " ++ printBoard xs n
@@ -170,7 +174,6 @@ EXAMPLES: printTop 0 = ""
 -}
 
 printTop :: Int -> String
-
 printTop 0 = []
 printTop n = (['a'..'z'] ++ ['A'..'Z'])!!(n-1) : " " ++ printTop (n-1)
 
@@ -185,11 +188,11 @@ EXAMPLES:
 
 
 printSquare :: Square -> String
-
-printSquare (Square _ Hidden _ _) = "#"
-printSquare (Square _ Revealed True n) = "*"
-printSquare (Square _ Revealed _ 0) = " "
-printSquare (Square _ Revealed _ n) = show n
+printSquare (Square _ _ _ _ True) = "F"
+printSquare (Square _ Hidden _ _ _) = "#"
+printSquare (Square _ Revealed True n _) = "*"
+printSquare (Square _ Revealed _ 0 _) = " "
+printSquare (Square _ Revealed _ n _) = show n
 
 
 {-dupMines mines
@@ -206,7 +209,6 @@ EXAMPLES: dupMines [] = False
 
 
 dupMines :: [Position] -> Bool
-
 dupMines mines = length remDups < length mines
   where remDups = map head $ group $ sort mines
 
@@ -222,7 +224,6 @@ EXAMPLES:
 
 
 getMines :: Int -> Int -> [Position] -> IO [Position]
-
 getMines 0 size mines = return mines
 getMines n size mines = do
 
@@ -249,9 +250,8 @@ EXAMPLES: countMines [] = 0
 
 
 countMines :: Board -> Int
-
 countMines [] = 0 
-countMines (square@(Square _ _ True _):s) = 1 + countMines s
+countMines (square@(Square _ _ True _ _):s) = 1 + countMines s
 countMines (square:s) = countMines s
 
 
@@ -268,114 +268,72 @@ EXAMPLES:
 
 
 getNeighbourSquares :: Position -> Board -> Int -> Board
-
 getNeighbourSquares (x,y) board size
-
   | x == 1  && y == 1  = [(getSquare (x,y+1) board),
-
                           (getSquare (x+1, y+1) board),
-
                           (getSquare (x+1, y) board)
-
                          ]
 
 
   | x == 1  && y == size = [(getSquare (x, y-1) board),
-
-                          (getSquare (x+1, y-1) board),
-
-                          (getSquare (x+1, y) board)
-
-                         ]
+                            (getSquare (x+1, y-1) board),
+                            (getSquare (x+1, y) board)
+                           ]
 
 
   | y == 1  && x == size = [(getSquare (x-1, y) board),
-
-                          (getSquare (x-1, y+1) board),
-
-                          (getSquare (x, y+1) board)
-
-                         ]
+                            (getSquare (x-1, y+1) board),
+                            (getSquare (x, y+1) board)
+                           ]
 
 
   | y == size && x == size = [(getSquare (x, y-1) board),
-
-                          (getSquare (x-1, y-1) board),
-
-                          (getSquare (x-1, y) board)
-
-                         ]
+                              (getSquare (x-1, y-1) board),
+                              (getSquare (x-1, y) board)
+                             ]
 
 
   | x == 1               = [(getSquare (x,y-1) board),
-
-                          (getSquare (x,y+1) board),
-
-                          (getSquare (x+1,y-1) board),
-
-                          (getSquare (x+1,y) board),
-
-                          (getSquare (x+1,y+1) board)
-
-                         ]
+                            (getSquare (x,y+1) board),
+                            (getSquare (x+1,y-1) board),
+                            (getSquare (x+1,y) board),
+                            (getSquare (x+1,y+1) board)
+                           ]
 
 
   | y == 1               = [(getSquare (x-1,y) board),
-
-                          (getSquare (x-1,y+1) board),
-
-                          (getSquare (x,y+1) board),
-
-                          (getSquare (x+1,y) board),
-
-                          (getSquare (x+1,y+1) board)
-
-                          ]
+                            (getSquare (x-1,y+1) board),
+                            (getSquare (x,y+1) board),
+                            (getSquare (x+1,y) board),
+                            (getSquare (x+1,y+1) board)
+                           ]
 
 
   | x == size            = [(getSquare (x-1,y-1) board),
-
-                         (getSquare (x-1,y) board),
-
-                         (getSquare (x-1,y+1) board),
-
-                         (getSquare (x,y-1) board),
-
-                         (getSquare (x,y+1) board) 
-
-                         ]
+                            (getSquare (x-1,y) board),
+                            (getSquare (x-1,y+1) board),
+                            (getSquare (x,y-1) board),
+                            (getSquare (x,y+1) board) 
+                           ]
 
 
   | y == size            = [(getSquare (x-1,y-1) board),
-
-                          (getSquare (x-1,y) board),
-
-                          (getSquare (x,y-1) board),
-
-                          (getSquare (x+1,y-1) board),
-
-                          (getSquare (x+1,y) board)
-
-                         ]
+                            (getSquare (x-1,y) board),
+                            (getSquare (x,y-1) board),
+                            (getSquare (x+1,y-1) board),
+                            (getSquare (x+1,y) board)
+                           ]
 
 
-  | otherwise =   [ (getSquare (x-1,y-1) board),
-
-                    (getSquare (x-1,y) board),
-
-                    (getSquare (x-1,y+1) board),
-
-                    (getSquare (x,y-1) board),
-
-                    (getSquare (x,y+1) board),
-
-                    (getSquare (x+1,y-1) board),
-
-                    (getSquare (x+1,y) board),
-
-                    (getSquare (x+1,y+1) board)
-
-                    ]
+  | otherwise =   [(getSquare (x-1,y-1) board),
+                   (getSquare (x-1,y) board),
+                   (getSquare (x-1,y+1) board),
+                   (getSquare (x,y-1) board),
+                   (getSquare (x,y+1) board),
+                   (getSquare (x+1,y-1) board),
+                   (getSquare (x+1,y) board),
+                   (getSquare (x+1,y+1) board)
+                  ]
 
 {- getSquare position board
 Get the corresponding square from the given position.
@@ -388,10 +346,8 @@ EXAMPLES:
 
 -}
 
-
 getSquare :: Position -> Board -> Square
-
-getSquare pos (square@(Square spos state _ _):s)
+getSquare pos (square@(Square spos _ _ _ _):s)
   | pos == spos = square
   | otherwise = getSquare pos s
 
@@ -405,7 +361,6 @@ Counts all mines of neighbours.
 
 
 countNeighbourMines :: Board -> Int
-
 countNeighbourMines [] = 0
 countNeighbourMines (neighbour:s) = (countMine neighbour) + countNeighbourMines s
 
@@ -414,551 +369,217 @@ countNeighbourMines (neighbour:s) = (countMine neighbour) + countNeighbourMines 
 -}
 
 -- If square has a mine, this will output 1 else 0
-
-
-
 countMine :: Square -> Int
-
-
-
-countMine (Square _ _ True _) = 1
-
-
-
+countMine (Square _ _ True _ _) = 1
 countMine _ = 0
 
 
-
-
-
-
-
 {- initNeighbours board boardcpy
-
-
-
    This function outputs a new board with NeighbourMines initialized for all squares.
-
-
 
 -}
 
-
-
 initNeighbours :: Board -> Board -> Int -> Board
-
-
-
 initNeighbours [] boardcpy _ = []
-
-
-
-initNeighbours board@(square@(Square pos state mine neighbours):s) boardcpy size =
-
-
-
-             (Square pos state mine (countNeighbourMines $ getNeighbourSquares pos boardcpy size): initNeighbours s boardcpy size)
-
-
-
-
-
+initNeighbours board@(square@(Square pos state mine neighbours flag):s) boardcpy size =
+             (Square pos state mine (countNeighbourMines $ getNeighbourSquares pos boardcpy size) flag : initNeighbours s boardcpy size)
 
 
 {- mkChoice position board
-
-
-
    This function takes a position and reveal the state of that square.
-
-
 
 -}
 
-
-
 mkChoice :: Maybe Position -> Board -> Board -> Int -> Board
-
-
-
 mkChoice _ [] cpy _ = cpy
-
-
-
-mkChoice (Just (x,y)) (square@(Square pos state mine mines):xs) cpy size
-
-
-
-  | (x,y) == pos && state == Hidden && mines == 0 = revealNeighbours (hiddenNeighbours $ getNeighbourSquares pos cpy size) (insertSquare (Square pos Revealed mine mines) cpy) size
-
-
-
-  | (x,y) == pos && state == Hidden = insertSquare (Square pos Revealed mine mines) cpy
-
-
-
+mkChoice (Just (x,y)) (square@(Square pos state mine mines flag):xs) cpy size
+  | (x,y) == pos && state == Hidden && mines == 0 = revealNeighbours (hiddenNeighbours $ getNeighbourSquares pos cpy size) (insertSquare (Square pos Revealed mine mines flag) cpy) size
+  | (x,y) == pos && state == Hidden = insertSquare (Square pos Revealed mine mines flag) cpy
   | otherwise = mkChoice (Just (x,y)) xs cpy size
-
 
 
 {- revealNeighbours COORDINATES!?!?!?  board size
 
-
-
 -}
 
-
-
 revealNeighbours :: [Square] -> Board -> Int -> Board
-
-
-
 revealNeighbours [] board _ = board
-
-
-
 revealNeighbours (x:xs) board size = revealNeighbours xs (revealNeighbours' x board size) size
 
 
-
-
-
 {-revealNeighbours' xxxxxxxxxxxxxxx-}
-
 revealNeighbours' :: Square -> Board -> Int -> Board
-
-
-
-revealNeighbours' (Square pos _ _ _) board size = mkChoice (Just pos) board board size
+revealNeighbours' (Square pos _ _ _ _) board size = mkChoice (Just pos) board board size
 
 
 
 {-insertSquare square@(Square pos1 _ _ _) (x@(Square pos2 _ _ _):xs)
 
-
-
-
-
 -}
 
-
-
-
-
 insertSquare :: Square -> Board -> Board
-
-
-
 insertSquare _ [] = []
-
-
-
-insertSquare square@(Square pos1 _ _ _) (x@(Square pos2 _ _ _):xs)
-
-
-
+insertSquare square@(Square pos1 _ _ _ _) (x@(Square pos2 _ _ _ _):xs)
   | pos1 == pos2 = square : xs
-
-
-
   | otherwise = x : insertSquare square xs
-
 
 
 {-getPosition (Square pos _ _ _)
 
-
-
 -}
-
-
-
 getPosition :: Square -> Position
-
-
-
-getPosition (Square pos _ _ _) = pos
-
+getPosition (Square pos _ _ _ _) = pos
 
 
 {-hiddenNeighbours neighbours
 
-
-
 -}
 
-
-
 hiddenNeighbours :: [Square] -> [Square]
-
-
-
 hiddenNeighbours [] = []
-
-
-
-hiddenNeighbours (x@(Square _ Hidden _ _):xs) = x : hiddenNeighbours xs
-
-
-
+hiddenNeighbours (x@(Square _ Hidden _ _ _):xs) = x : hiddenNeighbours xs
 hiddenNeighbours neighbours = hiddenNeighbours (tail neighbours)
 
 
-
-
-
-
-
 {- choiceMine position board
-
-
-
    Checks if given position has a mine on it, if it does, it returns true else false.
-
-
 
 -}
 
-
-
 choiceMine :: Maybe Position -> Board -> Bool
-
-
-
 choiceMine _ [] = False
-
-
-
 choiceMine Nothing board = False
-
-
-
-choiceMine (Just (x,y)) (square@(Square pos state mine mines):s)
-
-
-
+choiceMine (Just (x,y)) (square@(Square pos state mine mines flag):s)
   | (x,y) == pos && mine = mine
-
-
-
   | otherwise = choiceMine (Just (x,y)) s
 
 
-
-
-
-
+mkFlag :: Maybe Position -> Board -> Board
+mkFlag _ [] = []
+mkFlag Nothing board = board
+mkFlag (Just (x,y)) (square@(Square pos state mine mines flag):s)
+  | (x,y) == pos && flag == False = (Square pos state mine mines True) : mkFlag (Just (x,y)) s
+  | (x,y) == pos && flag == True = (Square pos state mine mines False) : mkFlag (Just (x,y)) s
+  | otherwise = square : mkFlag (Just (x,y)) s
 
 {- toPosition s
-
-
-
    This function translates a given move (ex. "1f") into its position (ex. (1,6))
-
-
 
 -}
 
-
-
 toPosition :: String -> Maybe Position
-
-
-
 toPosition (i1:i2:c:[]) = if isDigit i1 && isDigit i2 && isAlpha c then Just (read (i1 : i2 : []) :: Int, chartoInt c cilist) else Nothing
-
-
-
 toPosition (i:c:[]) = if isDigit i && isAlpha c then Just (digitToInt i, chartoInt c cilist) else Nothing
 
 
-
-toPosition _ = Nothing
-
-
-
-
-
-
-
 {- chartoInt
-
-
-
    Outputs the number that is mapped to the given character.
-
-
 
 -}
 
-
-
 chartoInt :: Char -> [(Char,Int)] -> Int
-
-
-
 chartoInt c (x:xs)
-
-
-
   | c == fst x = snd x
-
-
-
   | otherwise = chartoInt c xs
-
-
 
 {- cilist
 
-
-
-
-
-
-
 -}
-
-
-
 -- A list of tuples where characters are mappe to the corresponding numbers.
-
-
-
 cilist = zip (['a'..'z'] ++ ['A'..'Z']) [1..52]
 
 
-
-
-
-
-
-
-
-
-
 {- isWin board
-
-
-
    Checks if all squares are revealed (not counting the squares that has mines)
-
-
 
 -}
 
-
-
 isWin :: Board -> Bool
-
-
-
 isWin [] = True
-
-
-
-isWin (square@(Square pos state mine mines):s)
-
-
-
+isWin (square@(Square pos state mine mines flag):s)
   | state == Hidden && not mine = False
-
-
-
   | otherwise = isWin s
 
 
 
 
-
-
-
 {- play board size
-
-
-
    In this function we handle all the moves from the user,
-
-
-
    check if the game is over and prints the board. 
-
-
 
 -}
 
-
-
 play :: Board -> Int -> IO ()
-
-
-
 play board size = do
 
-
-
-
-
-
-
   putStrLn "Make your move"
-
-
-
-
-
-
-
   choice <- getLine
 
+  let newBoard = board
 
-
-
-
-
-
-  let newBoard = mkChoice (toPosition choice) board board size
-
-
-
-
-
-
-
-  prettyPrint $ printBoard newBoard size
-
-
-
-
-
-
-
-  if choiceMine (toPosition choice) board 
-
-
-
-    then putStrLn ("You Lost!\nYou hit a mine at: " ++ show choice) else if (isWin newBoard) then putStrLn "You won!"
-
-
-
-      else play newBoard size
-
-
-
-
-
+  if isPrefixOf "flag " choice then do
+    let newBoard = mkFlag (toPosition $ drop 5 choice) board
+    prettyPrint $ printBoard newBoard size
+    play newBoard size
+  else do
+    let newBoard = mkChoice (toPosition choice) board board size
+    prettyPrint $ printBoard newBoard size
+    if choiceMine (toPosition choice) board 
+      then putStrLn ("You Lost!\nYou hit a mine at: " ++ show choice) else if (isWin newBoard) then putStrLn "You won!"
+    else play newBoard size
 
 
 {- mkDifficulty s
-
-
-
    Outputs the size of the board and amount of mines depending on the desired difficulty
-
-
 
 -}
 
-
-
 mkDifficulty :: String -> IO (Int, Int)
-
-
-
 mkDifficulty "easy" = return (3,1)
-
-
-
 mkDifficulty "medium" = return (8,8)
-
-
-
 mkDifficulty "hard" = return (18,18)
-
-
-
 mkDifficulty "custom" = mkCustom
-
-
-
 mkDifficulty _ = getDifficulty
-
-
-
-
 
 {- getDifficulty
 
-
-
 -}
 
-
-
 getDifficulty :: IO (Int, Int)
-
 getDifficulty = do
 
   putStrLn "Please enter a difficulty (easy/medium/hard) or choose custom"
 
-
-
   stringDifficulty <- getLine
-
-
 
   difficulty <- mkDifficulty stringDifficulty
 
-
-
   return difficulty
-
-
-
-
 
 {-mkCustom
 
-
-
 -}
 
-
-
 mkCustom :: IO (Int, Int)
-
 mkCustom  = do
-
-
 
   boardChoice <- getBoardChoice
 
   let boardChoiceInt = (read boardChoice :: Int)
-
-  
 
   mineChoice <- getMineChoice boardChoiceInt
 
   let mineChoiceInt = (read boardChoice :: Int)
 
 
-
   return (boardChoiceInt, mineChoiceInt)
-
-
-
 
 
 {- getBoardChoice
 
-
-
 -}
 
-
-
 getBoardChoice :: IO String
-
 getBoardChoice = do
 
   putStrLn "Enter the size of the board"
@@ -981,20 +602,12 @@ getBoardChoice = do
 
   else return boardChoice
 
-  
-
-
 
 {- getMineChoice size
 
-
-
 -}
 
-
-
 getMineChoice :: Int -> IO String  
-
 getMineChoice size = do
 
   putStrLn "Enter the number of mines on the board"
@@ -1020,91 +633,33 @@ getMineChoice size = do
   else return mineChoice
 
 
-
-
-
 {- checkValidInt strInt
-
-
 
 -}
 
-
-
 checkValidInt :: String -> Bool
-
 checkValidInt strInt = isJust (readMaybe strInt :: Maybe Int)
 
 
-
-
-
 {- playMinesweeper
-
-
-
    Just testing stuff
-
-
-
 -}
 
-
-
 playMinesweeper :: IO ()
-
-
-
 playMinesweeper = do
 
-
-
-    putStrLn "Welcome to Minesweeper v.0.9"
-
-
-
-
+    putStrLn "Welcome to Minesweeper v.1"
 
     (size, mineAmount) <- getDifficulty
 
-
-
-
-
     let board = mkBoard size
-
-
-
-
-
-
 
     mines <- getMines mineAmount size []
 
-
-
-
-
-
-
     let newBoard = insertMines mines board
-
-
 
     let playBoard = initNeighbours newBoard newBoard size
 
-
-
-
-
-
-
     prettyPrint $ printBoard playBoard size
-
-
-
-
-
-
 
     play playBoard size
